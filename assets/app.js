5ef875cb67a3ff089a13fcf74c7daad467c8d813
Oliver Veras <55413049+acoymess@users.noreply.github.com>
2021-05-15 20:41:52 -0400

    Non-Responsive Skeleton complete

--- assets/app.js
+++ assets/app.js
@@ -1,9 +1,17 @@
 (function() {
 	"use strict";
-	toggleClass();
-
 	// Add Scripts Here
 
+	const showDetailsButton = document.querySelector('.show-details-button');
+	const detailsContainer = document.querySelector('.account-details-container');
+	const accountsContainer = document.querySelector('.accounts-container');
+	showDetailsButton.addEventListener("click", () => {
+		console.log("test");
+		showDetailsButton.classList.toggle('arrow-button-active');
+		accountsContainer.classList.toggle('accounts-container-active');
+		detailsContainer.classList.toggle('account-details-container-active');
+	});
 
-    	
+	
+
 })();
