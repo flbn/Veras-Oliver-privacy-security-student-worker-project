4128ffd24e513291edc487978b9ba0f281573539
Oliver Veras <55413049+acoymess@users.noreply.github.com>
2021-05-15 23:47:09 -0400

    Getting started on mobile, a little tired so make sure to check for mistakes

--- assets/app.js
+++ assets/app.js
@@ -6,12 +6,21 @@
 	const detailsContainer = document.querySelector('.account-details-container');
 	const accountsContainer = document.querySelector('.accounts-container');
 	showDetailsButton.addEventListener("click", () => {
-		console.log("test");
 		showDetailsButton.classList.toggle('arrow-button-active');
 		accountsContainer.classList.toggle('accounts-container-active');
 		detailsContainer.classList.toggle('account-details-container-active');
 	});
 
-	
+
+	const menuButton = document.querySelector('.menu-button');
+	const navLinks = document.querySelector('.mobile-nav-links');
+	menuButton.addEventListener("click", () => {
+		console.log("test");
+		menuButton.classList.toggle('arrow-button-active');
+		navLinks.classList.toggle('mobile-nav-links');
+		navLinks.classList.toggle('mobile-nav-links-active');
+	});
+
+
 
 })();
